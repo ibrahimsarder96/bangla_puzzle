341bbd5819ead43cdfb114135d045d051cde5d87
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2024-01-09 22:00:14 +0600

    cart item increase or decrese

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const listCartHTML = document.querySelector('.listCart');
 const listSingleCartHTML = document.querySelector('.singleCart');
 const iconCartSpan = document.querySelector('.icon-cart span');
 const foodItem = document.querySelector('.foodItem');
+const totalTaka = document.querySelector('.totalTaka span');
 
 let listFoods = [];
 let carts = [];
@@ -27,19 +28,20 @@ const addDataToHTML = () =>{
       newFood.classList.add('item');
       newFood.dataset.id = food.id;
       newFood.innerHTML = `
-      <img class="w-64 h-44" src="${food.image}" alt="">
-      <div>
-        <h1 class="text-xl font-medium">${food.name}</h1>
-        <p class="my-2">$ ${food.price}</p>
-        <p>${food.des}</p>
-      </div>
-      <button class="addCart py-3 mx-auto w-full bg-red-500 text-white text-xl font-medium my-4">Add to Order</button>
-      <button class="py-3 mx-auto w-full border-2 border-red-500 text-red-500 text-xl font-medium">Add to Order</button>
+    <img class="w-64 h-44" src="${food.image}" alt="">
+    <div>
+      <h1 class="text-xl font-medium">${food.name}</h1>
+      <p class="my-2">$ ${food.price}</p>
+      <p>${food.des}</p>
+    </div>
+    <button class="addCart py-3 mx-auto w-full bg-red-500 text-white text-xl font-medium my-4">Add to Order</button>
+    <button class="py-3 mx-auto w-full border-2 border-red-500 text-red-500 text-xl font-medium">Add to Order</button>
       `
       listFoodHTML.appendChild(newFood);
     })
   }
 }
+
 listFoodHTML.addEventListener('click', (event) =>{
   let positionClick = event.target;
   if(positionClick.classList.contains('addCart')){
@@ -71,10 +73,11 @@ const addToCart = (food_id) =>{
 const addCartToMemory = () =>{
   localStorage.setItem('cart', JSON.stringify(carts));
 }
-
+// food item cart add--------------------------
 const addCartToHTML = () =>{
   listSingleCartHTML.innerHTML = '';
   let totalQuantity = 0;
+  let taka = 0;
   if(carts.length > 0){
     carts.forEach(cart => {
       totalQuantity = totalQuantity + cart.quantity;
@@ -83,8 +86,9 @@ const addCartToHTML = () =>{
       newCart.dataset.id = cart.food_id;
       let positionFood = listFoods.findIndex((value) => value.id == cart.food_id);
       let info = listFoods[positionFood];
+      taka = info.price * totalQuantity;
       newCart.innerHTML = `
-      <div class=" relative mt-14 mx-5 flex items-center space-x-4 border-2 border-white rounded-md">
+      <div class=" relative mt-8 mx-5 flex items-center space-x-4 border-2 border-white rounded-md">
       <div>
         <img class="w-20 h-36 ml-2" src="images/food/img1.jpg" alt="">
       </div>
@@ -95,14 +99,14 @@ const addCartToHTML = () =>{
           <button>
             <span class="minus bg-gray-200 px-2 py-1 text-xl">-</span>
             </button>
-            <span class="bg-white py-1 px-4"></span>
+            <span class="bg-white py-1 px-4">${cart.quantity}</span>
             <button>
               <span class="plus bg-gray-200 px-2 py-1 text-xl">+</span>
               </button>
         </div>
         <p class="ml-16 mt-3 text-white">${info.price}</p>
       </div>
-      <i class="fa fa-trash-o mr-8 p-2 rounded-md bg-white text-red-500 absolute -right-4 -top-4" style="font-size:26px"></i>
+      <i class="removeCart pointer fa fa-trash-o mr-8 p-2 rounded-md bg-white text-red-500 absolute -right-4 -top-4" style="font-size:26px"></i>
 </div>
       `;
       listCartHTML.appendChild(newCart);
@@ -110,6 +114,7 @@ const addCartToHTML = () =>{
   }
   iconCartSpan.innerText = totalQuantity;
   foodItem.innerText = totalQuantity;
+  totalTaka.innerText = taka;
 }
 
 listCartHTML.addEventListener('click', (event)=>{
@@ -123,6 +128,7 @@ listCartHTML.addEventListener('click', (event)=>{
     changeQuantity(food_id, type);
   }
 })
+
 const changeQuantity = (food_id, type) =>{
   let positionItemInCart = carts.findIndex((value) => value.food_id == food_id);
   if(positionItemInCart >= 0){
